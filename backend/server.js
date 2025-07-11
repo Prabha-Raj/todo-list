2bb9e09d609ac8a913d8eba6779c5d3eabda1e3f
Prabha-Raj <prabhakarrajput86300@gmail.com>
2025-07-11 13:43:10 +0530

    af

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,10 @@ connectDB()
 const app = express()
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN, 
-  credentials: true
+  origin: "https://todoappbyprabha.netlify.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 app.use(express.json())
